ca8020d1cc5b26220dae6d76247127d8244a6091
hung.p.tran <hung.p.tran@opswat.com>
2022-10-23 08:49:42 +0700

    Reset code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { MinesweeperDrawer } from './implements/drawer';
-import { MinesweeperGame } from './implements/game';
-
-function main() {
-  const game = new MinesweeperGame(new MinesweeperDrawer());
-  game.start();
-}
+function main(){}
 
 main();
